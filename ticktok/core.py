1441b2454e093894bab220294b1bf3d95a61c007
elis <eli.segal@gmail.com>
2020-10-17 19:24:33 +0300

    added retry upon registering a clock

--- ticktok/core.py
+++ ticktok/core.py
@@ -4,6 +4,8 @@ from threading import Thread
 import requests
 from requests import HTTPError, ConnectionError
 
+from ticktok.http_session import create_retry_session
+
 DEFAULT_DOMAIN = 'http://localhost:9643'
 DEFAULT_TOKEN = 'ticktok-zY3wpR'
 
@@ -21,10 +23,11 @@ class TicktokClient:
         self._poll_url = None
         self._callback = None
         self._lock = threading.Lock()
+        self._http_session = create_retry_session()
 
     def register(self, name, schedule, callback):
         try:
-            response = requests.post(f'{self._url}/api/v1/clocks?access_token={self._token}',
+            response = self._http_session.post(f'{self._url}/api/v1/clocks?access_token={self._token}',
                                      json={'name': name, 'schedule': schedule})
             response.raise_for_status()
             self._callback = callback
